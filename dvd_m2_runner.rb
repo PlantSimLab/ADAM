0ee42864dcaa77e077c40dbee4e7cf38f7b26668
Franziska Hinkelmann <fhinkel@vt.edu>
2010-06-25 20:01:32 -0400

    almost connected

--- dvd_m2_runner.rb
+++ dvd_m2_runner.rb
@@ -9,10 +9,36 @@ p_value = ARGV[1]
 functionFile = ARGV[2] 
 
 puts "<br>"
-if (p_value != 2)
+puts functionFile
+if (p_value.to_i != 2)
   puts "This feature is only available for 2 states per node, not #{p_value}.<br>"
-  return 1
+  exit 1
 end
 
+puts "printing file #{functionFile}<br>"
+m2_system =  "{"
 
-return 0
+File.open( functionFile, 'r').each {|line|
+  puts "#{line}<br>"
+  m2_system = m2_system + line.split(/=/).last 
+  m2_system =  m2_system + ","
+}
+
+# remove last comma
+m2_system.chop!
+m2_system = m2_system + "};"
+
+puts "<br>"
+puts m2_system
+puts "<br>"
+
+
+
+
+
+
+exit 0
+
+###
+
+M2 solvebyGB.m2 --stop --no-debug --silent -q -e solvebyGB( { a,a+b}, booleanRing 2)
