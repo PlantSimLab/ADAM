560cc2c1452ad9a0ee63da103a20eb9ee4a0fb48
Franziska Hinkelmann <fhinkel@vt.edu>
2010-06-25 20:39:04 -0400

    running the system the user inputs and error check for n functions

--- dvd_m2_runner.rb
+++ dvd_m2_runner.rb
@@ -9,31 +9,38 @@ p_value = ARGV[1]
 functionFile = ARGV[2] 
 
 puts "<br>"
-puts functionFile
 if (p_value.to_i != 2)
   puts "This feature is only available for 2 states per node, not #{p_value}.<br>"
   exit 1
 end
 
-puts "printing file #{functionFile}<br>"
 m2_system =  "{"
 
+largestI = 0
 File.open( functionFile, 'r').each {|line|
-  puts "#{line}<br>"
-  m2_system = m2_system + line.split(/=/).last 
+  # puts "#{line}<br>"
+  ll = line.split(/=/)
+  m2_system = m2_system + ll.last 
   m2_system =  m2_system + ","
+  largestI = ll.first.split(/f/).last.to_i
 }
+
+if (largestI != n_nodes.to_i ) 
+  puts "There should be #{n_nodes} functions in order in the function
+  input, but the last funtion I read was f#{largestI}. Exiting. <br>"
+  exit 1
+end
 
 # remove last comma
 m2_system.chop!
-m2_system = m2_system + "};"
+m2_system = m2_system + "}"
 
 puts "<br>"
-puts m2_system
-puts "<br>"
-puts "running M2 now ...<br>"
+#puts m2_system
+#puts "<br>"
+puts "Running fixed point calculation now ...<br>"
 
-m2_result = `/Applications/Macaulay2-1.2/bin/M2 solvebyGB.m2 --stop --no-debug --silent -q -e 'QR = booleanRing 2; ll = gbSolver( { x1, x1+x2}, QR); exit 0'`
+m2_result = `/Applications/Macaulay2-1.2/bin/M2 solvebyGB.m2 --stop --no-debug --silent -q -e 'QR = booleanRing #{n_nodes}; ll = gbSolver( #{m2_system}, QR); exit 0'`
 
 puts m2_result
 puts "<br>"
