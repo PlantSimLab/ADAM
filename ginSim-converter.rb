ba17d3ff7bf1ba90dcd9b9e83c9dcf26d2a40898
Bonny Guang <bguang@hmc.edu>
2010-07-08 10:37:32 -0400

    fixed ginsim converter output

--- ginSim-converter.rb
+++ ginSim-converter.rb
@@ -11,17 +11,11 @@ end
 
 functionFile = ARGV[0] 
 
-m2_result = `cd lib/M2code/; M2 convertToPDS.m2 --stop --no-debug --silent -q -e 'toString converter("#{functionFile}"); exit 0'`
-
-#TODO: all the exponents disappear in Ruby b/c in M2 they're saved as type Expression
-#may possibly be easier to just make a function to make the string in M2
-
-#TODO: in addition, all the multiplication symbols are gone. this happens in M2 too.
-#I have no idea what's going on, since that doesn't make too much sense with the rings in M2
+m2_result = `cd lib/M2code/; M2 convertToPDS.m2 --stop --no-debug --silent -q -e 'print toString converter("#{functionFile}"); exit 0'`
 
 #get functions into array
 functions = m2_result.split("{")
-functions = functions.fetch(1).chop!
+functions = functions.fetch(1).chop!.chop!
 functions = functions.split(",")
 
 #make empty string for formatted functions
@@ -30,10 +24,9 @@ formatFuncts = String.new(str="")
 #format functions into f_i = ...
 for i in 0..(functions.length-1) do
   f = "f" + (i+1).to_s + " =" + functions.fetch(i)
-  formatFuncts = formatFuncts + " " + f + "\n"
+  formatFuncts = formatFuncts + " " + f + "<br>"
 end
 
-#TODO: this newline thing doesn't work for some reason
 puts formatFuncts
 
 exit 0
