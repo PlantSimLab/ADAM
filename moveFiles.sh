0a622ce79fbf52472f14a9a5b5bf5485be803b32
Franziska Hinkelmann <fhinkel@vt.edu>
2010-08-03 15:37:52 -0400

    little script to moe all the files to the correct location on the server

---
+++ moveFiles.sh
@@ -0,0 +1,6 @@
+cp ADAM_tut.html ../../htdocs/no-ssl/
+cp steptutorial.htm ../../htdocs/no-ssl/
+cp steptutorial_files/* ../../htdocs/no-ssl/steptutorial_files/
+cp adamv2.js ../../htdocs/no-ssl/
+cp adam_stylev2.css ../../htdocs/no-ssl/
+
