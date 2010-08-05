ec268e59c3c8c9d695426dffc35065a96931101f
Franziska Hinkelmann <bguang@hmc.edu>
2010-08-05 11:23:02 -0400

    correct files in move files

--- moveFiles.sh
+++ moveFiles.sh
@@ -1,6 +1,6 @@
 cp userGuide.html ../../htdocs/no-ssl/
 cp steptutorial.htm ../../htdocs/no-ssl/
 cp steptutorial_files/* ../../htdocs/no-ssl/steptutorial_files/
-cp adamv2.js ../../htdocs/no-ssl/
-cp adam_stylev2.css ../../htdocs/no-ssl/
+cp adam.js ../../htdocs/no-ssl/
+cp adam_style.css ../../htdocs/no-ssl/
 cp UserGuide_files/* ../../htdocs/no-ssl/UserGuide_files/
