d9e95a86c63e6f6115c993c927680538bfde341c
Franziska Hinkelmann <bguang@hmc.edu>
2010-08-05 00:55:36 -0400

    more files in moveFiles

--- moveFiles.sh
+++ moveFiles.sh
@@ -3,4 +3,4 @@ cp steptutorial.htm ../../htdocs/no-ssl/
 cp steptutorial_files/* ../../htdocs/no-ssl/steptutorial_files/
 cp adamv2.js ../../htdocs/no-ssl/
 cp adam_stylev2.css ../../htdocs/no-ssl/
-
+cp UserGuide_files/* ../../htdocs/no-ssl/UserGuide_files/
