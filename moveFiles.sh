719c0b2322623449f917d0f35a3abce277c0b8b1
Franziska Hinkelmann <fhinkel@vt.edu>
2010-09-11 12:11:55 -0400

    different links

--- moveFiles.sh
+++ moveFiles.sh
@@ -1,5 +1,3 @@
-cp userGuide.html ../../htdocs/no-ssl/
-cp steptutorial.htm ../../htdocs/no-ssl/
 cp steptutorial_files/* ../../htdocs/no-ssl/steptutorial_files/
 cp adam.js ../../htdocs/no-ssl/
 cp adam_style.css ../../htdocs/no-ssl/
