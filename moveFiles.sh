1153d1a2bf755486d4fb4adb260fe3837d6c079a
Franziska Hinkelmann <bguang@hmc.edu>
2010-09-11 12:27:35 -0400

    move script changed

--- moveFiles.sh
+++ moveFiles.sh
@@ -1,4 +1,5 @@
 cp steptutorial_files/* ../../htdocs/no-ssl/steptutorial_files/
 cp adam.js ../../htdocs/no-ssl/
+cp userGuide.php ../../htdocs/no-ssl/
 cp adam_style.css ../../htdocs/no-ssl/
 cp UserGuide_files/* ../../htdocs/no-ssl/UserGuide_files/
