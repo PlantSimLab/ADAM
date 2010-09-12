e055bdb6b55798a689c2dbda6a818321269a761e
Franziska Hinkelmann <bguang@hmc.edu>
2010-09-12 17:58:50 -0400

    move model files

--- moveFiles.sh
+++ moveFiles.sh
@@ -1,5 +1,5 @@
 cp steptutorial_files/* ../../htdocs/no-ssl/steptutorial_files/
+cp models/* ../../htdocs/no-ssl/models/
 cp adam.js ../../htdocs/no-ssl/
-cp userGuide.php ../../htdocs/no-ssl/
 cp adam_style.css ../../htdocs/no-ssl/
 cp UserGuide_files/* ../../htdocs/no-ssl/UserGuide_files/
