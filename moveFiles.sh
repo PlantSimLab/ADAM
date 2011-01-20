9f358601ed6f710cf1eb7d8f7cecf6a05bb9a545
Franziska Hinkelmann <fhinkel@vt.edu>
2011-01-20 13:36:16 -0500

    changing directories to reach repository.html

--- moveFiles.sh
+++ moveFiles.sh
@@ -6,6 +6,7 @@ cp adam.css ../../htdocs/no-ssl/
 cp UserGuide_files/* ../../htdocs/no-ssl/UserGuide_files/
 cp jquery-1.4.4.min.js ../../htdocs/no-ssl/jquery-1.4.4.min.js
 cp repository.js ../../htdocs/no-ssl/repository.js
+cp repository.html ../../htdocs/no-ssl/repository.html
 cp header.html ../../htdocs/no-ssl/header.html
 cp footer.html ../../htdocs/no-ssl/footer.html
 cp navigation.html ../../htdocs/no-ssl/navigation.html
