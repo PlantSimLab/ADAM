392fceb568116bbf4e71e744c9e6dda37209bc54
Franziska Hinkelmann <fhinkel@vt.edu>
2011-01-24 22:02:06 -0500

    another test

--- circuits.rb
+++ circuits.rb
@@ -76,8 +76,8 @@ m2_system = m2_system + "}"
 #puts "<br>"
 
 #one line is for my machine, one line is for the server b/c M2 is in different paths
-m2_result = `cd lib/M2code/; M2 functionalCircuits.m2 --stop --no-debug --silent -q -e 'printAdjacencyMatrix constructAdjacencyMatrix #{m2_system}; exit 0'`
-puts m2_result
+#m2_result = `cd lib/M2code/; M2 functionalCircuits.m2 --stop --no-debug --silent -q -e 'printAdjacencyMatrix constructAdjacencyMatrix #{m2_system}; exit 0'`
+#puts m2_result
 m2_result = `cd lib/M2code/; /usr/local/bin/M2 functionalCircuits.m2 --stop --no-debug --silent -q -e 'll = functionalCircuit #{m2_system}; stdio << length ll << "?" << circuitTable ll; exit 0'`
 temp = m2_result.split('?')
 numCircuits = temp.fetch(0)
