1ae36ae2459d6a95d9a3e23055c45dfed783e026
Seda Arat <bguang@hmc.edu>
2011-12-07 17:56:07 -0500

    unchanged

--- adam.pl
+++ adam.pl
@@ -351,12 +351,12 @@ given ($choice_box) {
 	            $tm_file = "$clientip.tm";
 	    
                 if ($upload_file_pm) {
-		say ("/usr/bin/perl SDDS.pl", "$filename_tt", "$initialState", "$interestingNodes", "$num_states", "$flag4ss", "$flag4tm", "$plot_file", "$histogram_file", "$tm_file", "$filename_pm", "<br>");
-		system ("/usr/bin/perl SDDS.pl", "$filename_tt", "$initialState", "$interestingNodes", "$num_states", "$flag4ss", "$flag4tm", "$plot_file", "$histogram_file", "$tm_file", "$filename_pm");
+		say ("perl SDDS.pl", "$filename_tt", "$initialState", "$interestingNodes", "$num_states", "$flag4ss", "$flag4tm", "$plot_file", "$histogram_file", "$tm_file", "$filename_pm", "<br>");
+		system ("perl SDDS.pl", "$filename_tt", "$initialState", "$interestingNodes", "$num_states", "$flag4ss", "$flag4tm", "$plot_file", "$histogram_file", "$tm_file", "$filename_pm");
 	      }
 	      else {
-		say ("/usr/bin/perl SDDS.pl", "$filename_tt", "$initialState", "$interestingNodes", "$num_states", "$flag4ss", "$flag4tm", "$plot_file", "$histogram_file", "$tm_file", "<br>");
-		system ("/usr/bin/perl SDDS.pl", "$filename_tt", "$initialState", "$interestingNodes", "$num_states", "$flag4ss", "$flag4tm", "$plot_file", "$histogram_file", "$tm_file");
+		say ("perl SDDS.pl", "$filename_tt", "$initialState", "$interestingNodes", "$num_states", "$flag4ss", "$flag4tm", "$plot_file", "$histogram_file", "$tm_file", "<br>");
+		system ("perl SDDS.pl", "$filename_tt", "$initialState", "$interestingNodes", "$num_states", "$flag4ss", "$flag4tm", "$plot_file", "$histogram_file", "$tm_file");
 	      }
 	    } # end of /SDDS/
 
