3494dd0602c70da11e62d8f6baf65982b739e6c5
Seda Arat <sedag@oinmathvtedu.local>
2012-01-24 16:54:55 -0500

    commented out the line /'removeCompleted = true/' for the pm_file of sdds

--- adam.js
+++ adam.js
@@ -53,7 +53,9 @@ $(document).ready(function() {
     });
 
     // filePM is for propensity matrix in SDDS
+
     var filePM = "";
+
     $('#upload_file_pm').uploadify({
 	'uploader': '/uploadify/uploadify.swf',
         'script': '/uploadify/uploadify.php',
@@ -61,7 +63,7 @@ $(document).ready(function() {
         'folder': '/../../htdocs/no-ssl/files',
         'auto': false,
         'multi': false,
-	'removeCompleted' : true,
+	//'removeCompleted' : true,
     'onSelect': function(event, ID, fileObj) {
             filePM = fileObj.name;
             //alert('The file ' + fileObj.name + ' was added to the queue.');
