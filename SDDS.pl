85678ff4d694ca999d8ed9b3da7a636a54fdc859
Seda Arat <sedag@oinmathvtedu.local>
2012-03-29 14:13:47 -0400

    in graphs, transparent is set to 0 and bgclr is set to white

--- SDDS.pl
+++ SDDS.pl
@@ -211,6 +211,8 @@ for (my $a = 0; $a < $sdds->max_num_interestingNodes(); $a++) {
 my $graph = GD::Graph::linespoints->new(900, 500);
 $graph -> set_legend(@legend_keys);
 $graph -> set (
+	       transparent => 0,
+	       bgclr => qw (white),
 	       x_label => 'Time Steps',
 	       y_label => "Average Expression Level",
 	       title => 'Cell Population Simulation',
@@ -267,6 +269,8 @@ if ($s) {
   
   my $histogram = GD::Graph::bars->new(900, 500);
   $histogram -> set (
+		     transparent => 0,
+		     bgclr => qw (white),
 		     x_label => 'States',
 		     y_label => 'Percentages',
 		     title => 'Probability Distribution',
